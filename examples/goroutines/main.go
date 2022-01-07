a9c69338af9e4894005061ee2f38939636985b47
esendjer <esendjer@gmail.com>
2022-01-07 13:17:24 +0500

    fixed the problem with random panic on a call Py_Finalize due to not right working with references and its decrement

--- examples/goroutines/main.go
+++ examples/goroutines/main.go
@@ -12,27 +12,25 @@ import (
 func main() {
 	var err error
 
-	// At the end of the main function execution
-	// it will check, print and exit with non-zero code if any error is here
+	//  At the end of the main function execution
+	//  it will check, print and exit with non-zero code if any error is here
 	defer func() {
 		if err != nil {
 			fmt.Println(err)
 			os.Exit(1)
 		}
 	}()
-	// it will undo all initializations made by Py_Initialize()
+	//  it will undo all initializations made by Py_Initialize()
 	defer python3.Py_Finalize()
-	// it will print any python error if it is here
-	//   no needs to call it after each single check of PyErr_Occurred()
+	//  it will print any python error if it is here
+	//    no needs to call it after each single check of PyErr_Occurred()
 	defer python3.PyErr_Print()
 
-	// The following will also create the GIL explicitly
-	// by calling PyEval_InitThreads(), without waiting
-	// for the interpreter to do that
+	//  Initialize the Python interpreter and create the GIL explicitly
 	python3.Py_Initialize()
 
 	if !python3.Py_IsInitialized() {
-		err = errors.New("Error initializing the python interpreter")
+		err = errors.New("error initializing the python interpreter")
 		return
 	}
 
@@ -40,55 +38,53 @@ func main() {
 	wg.Add(2)
 
 	nameModule := "foo"
-	fooModule := python3.PyImport_ImportModule(nameModule)
+	fooModule := python3.PyImport_ImportModule(nameModule) //  New reference
 	if fooModule == nil && python3.PyErr_Occurred() != nil {
-		err = errors.New("Error importing the python module")
-		fooModule.DecRef()
+		err = errors.New("error importing the python module")
 		return
 	}
+	defer fooModule.DecRef()
 
 	oddsAttr := "print_odds"
 	evenAttr := "print_even"
-
-	odds := fooModule.GetAttrString(oddsAttr)
+	odds := fooModule.GetAttrString(oddsAttr) //  New reference
 	if odds == nil && python3.PyErr_Occurred() != nil {
-		err = errors.New("Error getting the attribute print_odds")
-		odds.DecRef()
+		err = errors.New("error getting the attribute print_odds")
 		return
 	}
+	defer odds.DecRef()
 
-	even := fooModule.GetAttrString(evenAttr)
+	even := fooModule.GetAttrString(evenAttr) //  New reference
 	if even == nil && python3.PyErr_Occurred() != nil {
-		err = errors.New("Error getting the attribute print_even")
-		even.DecRef()
+		err = errors.New("error getting the attribute print_even")
 		return
 	}
+	defer even.DecRef()
 
-	limit := python3.PyLong_FromGoInt(50)
+	limit := python3.PyLong_FromGoInt(50) //  New reference, will stolen later
 	if limit == nil && python3.PyErr_Occurred() != nil {
-		err = errors.New("Error creating python long object")
-		limit.DecRef()
+		err = errors.New("error creating python long object")
 		return
 	}
 
-	args := python3.PyTuple_New(1)
+	args := python3.PyTuple_New(1) //  New reference
 	if args == nil && python3.PyErr_Occurred() != nil {
-		err = errors.New("Error creating python tuple object")
-		args.DecRef()
+		err = errors.New("error creating python tuple object")
 		return
 	}
+	defer args.DecRef()
 
-	ret := python3.PyTuple_SetItem(args, 0, limit)
+	ret := python3.PyTuple_SetItem(args, 0, limit) //  Steals reference to limit
+	limit = nil
 	if ret != 0 {
-		args.DecRef()
+		err = errors.New("error setting a tuple item")
 		limit.DecRef()
-		err = errors.New("Error setting a tuple item")
+		limit = nil
 		return
 	}
 
-	// Py_Initialize() has locked the the GIL but at this point we don't need it
-	// anymore. We save the current state and release the lock
-	// so that goroutines can acquire it
+	//  Save the current state and release the lock
+	//  so that goroutines can acquire it
 	state := python3.PyEval_SaveThread()
 
 	go func() {
@@ -109,8 +105,6 @@ func main() {
 
 	wg.Wait()
 
-	// At this point we know we won't need python anymore in this
-	// program, we can restore the state and lock the GIL to perform
-	// the final operations before exiting.
+	//  Restore the state and lock the GIL
 	python3.PyEval_RestoreThread(state)
 }
