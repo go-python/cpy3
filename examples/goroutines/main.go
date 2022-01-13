2f094fc65f804b2a940a0d4418d3bf1daa6f6d7f
esendjer <esendjer@gmail.com>
2022-01-13 16:23:57 +0500

    Fixed comment. Reached to idiomatic as possible. Applied linters.

--- examples/goroutines/main.go
+++ examples/goroutines/main.go
@@ -12,22 +12,26 @@ import (
 func main() {
 	var err error
 
-	//  At the end of the main function execution
-	//  it will check, print and exit with non-zero code if any error is here
+	// At the end of all, if there was an error
+	// prints the error and exit with the non-zero code
 	defer func() {
 		if err != nil {
 			fmt.Println(err)
 			os.Exit(1)
 		}
 	}()
-	//  it will undo all initializations made by Py_Initialize()
+
+	// Undo all initializations made by Py_Initialize() and subsequent
 	defer python3.Py_Finalize()
-	//  it will print any python error if it is here
-	//    no needs to call it after each single check of PyErr_Occurred()
+
+	// Prints any python error if it was here
+	// no needs to call it after each single check of PyErr_Occurred()
 	defer python3.PyErr_Print()
 
-	//  Initialize the Python interpreter and create the GIL explicitly
-	python3.Py_Initialize()
+	// Initialize the Python interpreter and
+	// since version 3.7 it also create the GIL explicitly by calling PyEval_InitThreads()
+	// so you don’t have to call PyEval_InitThreads() yourself anymore
+	python3.Py_Initialize() // create the GIL, the GIL is locked by the main thread
 
 	if !python3.Py_IsInitialized() {
 		err = errors.New("error initializing the python interpreter")
@@ -37,45 +41,46 @@ func main() {
 	var wg sync.WaitGroup
 	wg.Add(2)
 
-	nameModule := "foo"
-	fooModule := python3.PyImport_ImportModule(nameModule) //  New reference
+	fooModule := python3.PyImport_ImportModule("foo") // new reference, a call DecRef() is needed
 	if fooModule == nil && python3.PyErr_Occurred() != nil {
 		err = errors.New("error importing the python module")
 		return
 	}
 	defer fooModule.DecRef()
 
-	oddsAttr := "print_odds"
-	evenAttr := "print_even"
-	odds := fooModule.GetAttrString(oddsAttr) //  New reference
+	odds := fooModule.GetAttrString("print_odds") // new reference, a call DecRef() is needed
 	if odds == nil && python3.PyErr_Occurred() != nil {
 		err = errors.New("error getting the attribute print_odds")
 		return
 	}
 	defer odds.DecRef()
 
-	even := fooModule.GetAttrString(evenAttr) //  New reference
+	even := fooModule.GetAttrString("print_even") // new reference, a call DecRef() is needed
 	if even == nil && python3.PyErr_Occurred() != nil {
 		err = errors.New("error getting the attribute print_even")
 		return
 	}
 	defer even.DecRef()
 
-	limit := python3.PyLong_FromGoInt(50) //  New reference, will stolen later
+	limit := python3.PyLong_FromGoInt(50) // new reference, will stolen later, a call DecRef() is NOT needed
 	if limit == nil && python3.PyErr_Occurred() != nil {
 		err = errors.New("error creating python long object")
 		return
 	}
 
-	args := python3.PyTuple_New(1) //  New reference
+	args := python3.PyTuple_New(1) // new reference, a call DecRef() is needed
 	if args == nil && python3.PyErr_Occurred() != nil {
 		err = errors.New("error creating python tuple object")
 		return
 	}
 	defer args.DecRef()
 
-	ret := python3.PyTuple_SetItem(args, 0, limit) //  Steals reference to limit
+	ret := python3.PyTuple_SetItem(args, 0, limit) // steals reference to limit
+
+	// Cleans the Go variable, because now a new owner is caring about related PyObject
+	// no action, such as a call DecRef(), is needed here
 	limit = nil
+
 	if ret != 0 {
 		err = errors.New("error setting a tuple item")
 		limit.DecRef()
@@ -83,28 +88,28 @@ func main() {
 		return
 	}
 
-	//  Save the current state and release the lock
-	//  so that goroutines can acquire it
-	state := python3.PyEval_SaveThread()
+	// Save the current state and release the GIL
+	// so that goroutines can acquire it
+	state := python3.PyEval_SaveThread() // release the GIL, the GIL is unlocked for using by goroutines
 
 	go func() {
-		_gstate := python3.PyGILState_Ensure()
+		_gstate := python3.PyGILState_Ensure() // acquire the GIL, the GIL is locked by the 1st goroutine
 		odds.Call(args, python3.PyDict_New())
-		python3.PyGILState_Release(_gstate)
+		python3.PyGILState_Release(_gstate) // release the GIL, the GIL is unlocked for using by others
 
 		wg.Done()
 	}()
 
 	go func() {
-		_gstate := python3.PyGILState_Ensure()
+		_gstate := python3.PyGILState_Ensure() // acquire the GIL, the GIL is locked by the 2nd goroutine
 		even.Call(args, python3.PyDict_New())
-		python3.PyGILState_Release(_gstate)
+		python3.PyGILState_Release(_gstate) // release the GIL, the GIL is unlocked for using by others
 
 		wg.Done()
 	}()
 
 	wg.Wait()
 
-	//  Restore the state and lock the GIL
-	python3.PyEval_RestoreThread(state)
+	// Restore the state and lock the GIL
+	python3.PyEval_RestoreThread(state) // acquire the GIL, the GIL is locked by the main thread
 }
